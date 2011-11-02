689a3f7e76fcda6733b851e287e4716f92be14a7
Deven Fan <devenfan@gmail.com>
2011-11-02 22:00:27 +0800

    iButton GPIO Test app

---
+++ android_ibutton_gpio/src/net/sh/android/gpio/Android_ibutton_gpioActivity.java
@@ -0,0 +1,13 @@
+package net.sh.android.gpio;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Android_ibutton_gpioActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
